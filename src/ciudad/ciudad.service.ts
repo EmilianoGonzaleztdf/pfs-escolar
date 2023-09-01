8f826df39e8b3b8b865ff99db59374567c136f00
EmilianoGonzaleztdf <emilianogonzalez@tdf.edu.ar>
2023-08-31 21:56:40 -0300

    hecho en clase

--- src/ciudad/ciudad.service.ts
+++ src/ciudad/ciudad.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Ciudad } from './entities/ciudad.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CiudadService {}
+export class CiudadService {
+  private ciudades: Ciudad[] = [];
+
+  constructor(
+    @InjectRepository(Ciudad)
+    private ciudadRepository: Repository<Ciudad>,
+  ) {}
+
+ async findAllRaw() : Promise<Ciudad[]> {
+  this.ciudades = [];
+    let data = await this.ciudadRepository.query('select * from ciudad');
+
+    data.forEach((element) => {
+      let ciudad: Ciudad = new Ciudad(element.nombre);
+      this.ciudades.push(ciudad);
+    });
+    return this.ciudades;
+  }
+  async findAllOrm():Promise<Ciudad[]>{
+    return await this.ciudadRepository.find(); 
+  }
+}
